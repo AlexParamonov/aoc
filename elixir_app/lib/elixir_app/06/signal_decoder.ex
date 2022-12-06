a92f0b70d1e879698365ebcda80c6f3dcb3a41fa
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2022-12-06 14:38:52 +0100

    [Day 6] Add elixir message start puzzle 2/2

--- elixir_app/lib/elixir_app/06/signal_decoder.ex
+++ elixir_app/lib/elixir_app/06/signal_decoder.ex
@@ -1,8 +1,14 @@
 defmodule ElixirApp.SignalDecoder do
-  def message_start(input) do
+  def packet_start(input) do
     input
     |> String.to_charlist()
     |> find_marker_index()
+  end
+
+  def message_start(input) do
+    input
+    |> String.to_charlist()
+    |> find_marker_index(14)
   end
 
   @spec find_marker_index(list(char)) :: integer
@@ -10,6 +16,7 @@ defmodule ElixirApp.SignalDecoder do
     Enum.reduce_while(list, 0, fn _char, index ->
       possible_marker = Enum.slice(list, index..(index + marker_length - 1))
       cond do
+        Enum.count(possible_marker) < marker_length -> {:halt, nil}
         possible_marker == Enum.uniq(possible_marker) -> {:halt, index + marker_length}
         true -> {:cont, index + 1}
       end
