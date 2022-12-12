057bfb2bf4e42d9af4948291e64f2eae3d8b9e4e
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2022-12-12 19:56:13 +0100

    [Day 12] Add both pathfinder puzzles (ugly and with a lib)

--- elixir_app/mix.exs
+++ elixir_app/mix.exs
@@ -27,6 +27,7 @@ defmodule ElixirApp.MixProject do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:math, "~> 0.6.0"},
+      {:libgraph, "~> 0.7"},
       {:flow_assertions, "~> 0.6", only: :test},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
