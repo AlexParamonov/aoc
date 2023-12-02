9770f4094a7804405409da3de13848f48ba16519
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2023-12-02 13:23:59 +0100

    [Day 2] mixmax rgb 2/2

--- 2023-elixir_app/lib/elixir_app/02/cube_game.ex
+++ 2023-elixir_app/lib/elixir_app/02/cube_game.ex
@@ -3,6 +3,14 @@ defmodule ElixirApp.CubeGame do
     defstruct [:id, :iterations]
   end
 
+  def minmax_power_sum(raw_input) do
+    raw_input
+    |> parse_list
+    |> load_games
+    |> find_minmax_power
+    |> Enum.sum()
+  end
+
   def validate_games(raw_input, guess) do
     raw_input
     |> parse_list
@@ -15,6 +23,30 @@ defmodule ElixirApp.CubeGame do
   defp load_games(raw_games) do
     raw_games
     |> Enum.map(&load_game/1)
+  end
+
+  defp find_minmax_power(games) do
+    games
+    |> Enum.map(fn game ->
+      game
+      |> max_rgb()
+      |> power()
+    end)
+  end
+
+  defp max_rgb(game) do
+    game.iterations
+    |> Enum.reduce(%{red: 0, green: 0, blue: 0}, fn rgb, acc ->
+      Map.merge(acc, rgb, fn _, current_max_rgb, rgb ->
+        max(rgb, current_max_rgb)
+      end)
+    end)
+  end
+
+  defp power(rgb) do
+    Enum.reduce(rgb, 1, fn {_k, count}, acc ->
+      acc * count
+    end)
   end
 
   defp filter_games(games, guess) do
@@ -45,6 +77,7 @@ defmodule ElixirApp.CubeGame do
 
       {String.to_existing_atom(color), String.to_integer(number)}
     end)
+    |> Map.new()
   end
 
   defp valid?(game, guess) do
--- 2023-elixir_app/test/elixir_app/02/cube_game_test.exs
+++ 2023-elixir_app/test/elixir_app/02/cube_game_test.exs
@@ -18,6 +18,11 @@ defmodule ElixirApp.CubeGameTest do
     end
   end
 
+  describe ".minmax_power_sum" do
+    test "finds min possible set of cubes, multiplies them and sums results of all games", %{raw_input: raw_input}  do
+      assert 2_286 == CubeGame.minmax_power_sum(raw_input)
+    end
+  end
 
   describe "result" do
     setup do
@@ -29,7 +34,11 @@ defmodule ElixirApp.CubeGameTest do
     test "returns a sum of possible game ids for a given guess", %{raw_input: raw_input} do
       guess = %{red: 12, green: 13, blue: 14}
 
-      assert 8 == CubeGame.validate_games(raw_input, guess)
+      assert 1_853 == CubeGame.validate_games(raw_input, guess)
+    end
+
+    test "finds min possible set of cubes, multiplies them and sums results of all games", %{raw_input: raw_input}  do
+      assert 72_706 == CubeGame.minmax_power_sum(raw_input)
     end
   end
 end
