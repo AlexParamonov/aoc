f973c36cc57c6e3b3acd846a4f2ef4601be5e3e1
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2023-12-03 15:01:49 +0100

    [Day 3] engine part calc (wrong numbers calc)

---
+++ 2023-elixir_app/lib/elixir_app/03/engine.ex
@@ -0,0 +1,135 @@
+defmodule ElixirApp.Engine do
+  # TODO 0..9
+  @numbers ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+  def sum_parts(raw_input) do
+    matrix =
+      raw_input
+      |> load_matrix
+
+    adjacent_positions =
+      matrix
+      |> build_adjacent_positions
+
+    matrix
+    |> find_orphans(adjacent_positions)
+    |> Enum.sum()
+  end
+
+  def load_matrix(raw_input) do
+    raw_input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, "", trim: true))
+  end
+
+  def build_adjacent_positions(matrix) do
+    matrix
+    |> find_symbol_positions
+    |> expand_positions
+    |> filter_empty_positions(matrix)
+    |> adjust_for_starting_position(matrix)
+    |> Enum.uniq()
+  end
+
+  defp find_symbol_positions(matrix) do
+    matrix
+    |> Enum.with_index()
+    |> Enum.reduce([], fn {row, row_index}, acc ->
+      row
+      |> Enum.with_index()
+      |> Enum.reduce([], fn
+        {value, _index}, row_acc when value in ["." | @numbers] ->
+          row_acc
+
+        {_value, index}, row_acc ->
+          [{row_index, index} | row_acc]
+      end)
+      |> Kernel.++(acc)
+    end)
+  end
+
+  defp expand_positions(positions) do
+    positions
+    |> Enum.map(fn {row, index} ->
+      [
+        {row, index - 1},
+        {row, index + 1},
+        {row + 1, index},
+        {row + 1, index + 1},
+        {row + 1, index - 1},
+        {row - 1, index + 1},
+        {row - 1, index - 1},
+        {row - 1, index}
+      ]
+      |> Enum.filter(fn
+        {-1, _index} -> false
+        {_row, -1} -> false
+        _ -> true
+      end)
+    end)
+    |> List.flatten()
+  end
+
+  defp filter_empty_positions(positions, matrix) do
+    positions
+    |> Enum.filter(fn {row_index, index} ->
+      "." !=
+        matrix
+        |> Enum.at(row_index)
+        |> Enum.at(index)
+    end)
+  end
+
+  defp adjust_for_starting_position(positions, matrix) do
+    positions
+    |> Enum.map(fn {row_index, index} ->
+      matrix
+      |> Enum.at(row_index)
+      |> Enum.at(index)
+      |> shift_position_left(row_index, index, matrix)
+    end)
+  end
+
+  defp shift_position_left(_value, row_index, -1, _matrix), do: {row_index, 0}
+
+  defp shift_position_left(value, row_index, index, matrix) when value in @numbers do
+    new_index = index - 1
+
+    new_value =
+      matrix
+      |> Enum.at(row_index)
+      |> Enum.at(new_index)
+
+    shift_position_left(new_value, row_index, new_index, matrix)
+  end
+
+  defp shift_position_left(value, row_index, index, matrix), do: {row_index, index + 1}
+
+  def find_orphans(matrix, adjacent_positions) do
+    matrix
+    |> numbers_positions
+    |> Enum.reject(fn {position, _length} ->
+      position in adjacent_positions
+    end)
+    |> Enum.map(fn {{row_index, index}, length} ->
+      matrix
+      |> Enum.at(row_index)
+      |> Enum.slice(index, length)
+      |> Enum.join()
+      |> String.to_integer()
+    end)
+  end
+
+  defp numbers_positions(matrix) do
+    matrix
+    |> Enum.with_index()
+    |> Enum.map(fn {row, row_index} ->
+      Regex.scan(~r/\d+/, Enum.join(row), return: :index)
+      |> List.flatten()
+      |> Enum.map(fn {index, length} ->
+        {{row_index, index}, length}
+      end)
+    end)
+    |> List.flatten()
+  end
+end
---
+++ 2023-elixir_app/test/elixir_app/03/engine_test.exs
@@ -0,0 +1,58 @@
+defmodule ElixirApp.EngineTest do
+  use ExUnit.Case, async: true
+
+  alias ElixirApp.Engine
+  alias ElixirApp.FileFixtures
+
+  setup do
+    raw_input = FileFixtures.content("03/demo_engine.txt")
+
+    %{raw_input: raw_input}
+  end
+
+  describe ".sum_parts" do
+    test "returns a sum of parts numbers near symbol", %{raw_input: raw_input} do
+      assert 4361 == Engine.sum_parts(raw_input)
+    end
+  end
+
+  describe ".load_matrix" do
+    test "returns a matrix from raw input" do
+      expected = [
+        ["1", ".", "3"],
+        [".", ".", "6"],
+        ["7", "8", "."]
+      ]
+
+      raw_input = "1.3\n..6\n78."
+
+      assert expected == Engine.load_matrix(raw_input)
+    end
+  end
+
+  describe ".build_adjacent_positions" do
+    test "returs a row and index of the beginning of adjacent_position" do
+      matrix = [
+        ["1", "#", "3"],
+        ["2", "5", "."],
+        ["7", "8", "."]
+      ]
+
+      expected = [
+        {0, 0},
+        {0, 2},
+        {1, 0}
+      ]
+
+      assert expected == Engine.build_adjacent_positions(matrix)
+    end
+  end
+
+  describe "result" do
+    setup do
+      raw_input = FileFixtures.content("03/engine.txt")
+
+      %{raw_input: raw_input}
+    end
+  end
+end
